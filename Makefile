60f68e831ee8a96bfde28102b55dd99b8bbcf8da
oojahooo <oojahooo@gmail.com>
2022-11-20 12:22:52 +0900

    [Init] initialize patron project

---
+++ Makefile
@@ -0,0 +1,17 @@
+MAKE=@make
+DUNE=@dune
+LN=@ln -sf
+RM=@rm
+EXE=patron
+
+all:
+	$(DUNE) build src/main.exe
+	$(LN) _build/default/src/main.exe $(EXE)
+
+test: all
+	$(MAKE) -C test
+	$(DUNE) test
+
+clean:
+	$(DUNE) clean
+	$(RM) -rf $(EXE)
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+export OPAMYES=1
+
+NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
+OCAML_VERSION="4.13.1"
+PATRON_OPAM_SWITCH=patron-"$OCAML_VERSION"
+opam init --reinit --bare --no-setup
+
+switch_exists=no
+for installed_switch in $(opam switch list --short); do
+  if [[ "$installed_switch" == "$PATRON_OPAM_SWITCH" ]]; then
+    switch_exists=yes
+    break
+  fi
+done
+
+if [ "$switch_exists" = "no" ]; then
+  opam switch create $PATRON_OPAM_SWITCH $OCAML_VERSION
+else
+  opam switch $PATRON_OPAM_SWITCH
+fi
+
+eval $(SHELL=bash opam config env --switch=$PATRON_OPAM_SWITCH)
+opam install -j $NCPU dune z3 core yojson
+opam install -j $NCPU ocamlformat merlin ocp-index ocp-indent  # for development
+make
