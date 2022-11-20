d3889d40bec41de81865262e0528220d7017a940
oojahooo <oojahooo@gmail.com>
2022-11-20 21:48:33 +0900

    [Src] add some datatypes (need to complete)

--- src/patmat.ml
+++ src/patmat.ml
@@ -20,6 +20,22 @@ let solver =
   Z3.Fixedpoint.set_parameters s param;
   s
 
+module AST = struct
+  (* TODO: pick necessary ast nodes as logical relations in clang ast *)
+  type t = TranslationUnitDecl | TypedefDecl | FunctionDecl | CompoundStmt
+end
+
+module Node = struct
+  type t = { id : int; name : string; line : int }
+end
+
+module Dataflow = struct
+  type t = DUEdge of Node.t * Node.t
+end
+
+(* TODO: make facts from given ast & dataflow *)
+let mk_facts (ast : AST.t) (dataflow : Dataflow.t) = ()
+
 let extract_bug_pattern () =
   (* Hard coding for the time being *)
   ()
