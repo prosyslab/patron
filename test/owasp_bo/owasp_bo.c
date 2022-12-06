89f42dfd96366121971a2d70fe93f3e1eab63d7b
oojahooo <oojahooo@gmail.com>
2022-12-06 20:42:49 +0900

    [Test] add c program (OWASP_TUTORIAL_BO)

---
+++ test/owasp_bo/owasp_bo.c
@@ -0,0 +1,17 @@
+char* getenv(const char *__name);
+void sprintf(char *str, char *fmt, ...);
+void printf(const char *, ...);
+
+int main(void) {
+  char *ptr_h;
+  char h[64];
+
+  ptr_h = getenv("HOME"); // src
+  if (ptr_h != (void *) 0) {
+    sprintf(h, "Your home directory is: %s !", ptr_h); // BO // snk
+    printf("%s\n", h);
+  }
+
+  return 0;
+}
+
---
+++ test/owasp_bo/owasp_bo_patch.c
@@ -0,0 +1,17 @@
+char* getenv(const char *__name);
+void snprintf(char *str, char *fmt, ...);
+void printf(const char *, ...);
+
+int main(void) {
+  char *ptr_h;
+  char h[64];
+
+  ptr_h = getenv("HOME"); // src
+  if (ptr_h != (void *) 0) {
+    snprintf(h, sizeof(h), "Your home directory is: %s !", ptr_h); // BO // snk
+    printf("%s\n", h);
+  }
+
+  return 0;
+}
+
