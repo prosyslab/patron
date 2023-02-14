0fb13f571618296a79f7996bbf795a26dee9accc
oojahooo <oojahooo@gmail.com>
2023-02-14 17:50:35 +0900

    [Transformer] add basic frame

--- src/transformer.ml
+++ src/transformer.ml
@@ -6,9 +6,23 @@ type edit_script =
   | DeleteInstr of Cil.instr
   | UpdateInstr of Cil.instr * Cil.instr
   | UpdateExp of Cil.exp * Cil.exp
+  | UpdateLval of Cil.lval * Cil.lval
 
 let extract cil1 cil2 = ()
-let translate cil1 cil3 es = ()
+let translate cil1 cil3 es = es
 let apply es cil3 = cil3
-let main () = ()
+
+let main () =
+  Cil.initCIL ();
+  Cil.insertImplicitCasts := false;
+  let donor, patch, donee =
+    match Array.to_list Sys.argv |> List.tl with
+    | [ a; b; c ] -> (Frontc.parse a (), Frontc.parse b (), Frontc.parse c ())
+    | _ -> failwith "Argument mismatch: must be <Donor> <Patch> <Donee>"
+  in
+  let es = extract donor patch in
+  let es' = translate donor donee es in
+  let patched_donee = apply es' donee in
+  Cil.dumpFile !Cil.printerForMaincil stdout "" patched_donee
+
 let _ = main ()
